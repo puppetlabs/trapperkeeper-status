edb20b3519977080d2447691115a027f2e8797d8
Ruth Linehan <ruth@puppetlabs.com>
2015-04-27 15:21:17 -0700

    (maint) Handle non-user-data-related schema errors
    Validate that the functions used for returning the status from a
    service/services have correct inputs and outputs and, if not, return a
    well-formatted HTTP error.

--- src/puppetlabs/trapperkeeper/services/status/status_core.clj
+++ src/puppetlabs/trapperkeeper/services/status/status_core.clj
@@ -45,23 +45,24 @@
   (let [status-map (service-status-map svc-version status-version status-fn)]
     (swap! status-fns-atom update-in [svc-name] conj status-map)))
 
-(schema/defn call-latest-status-fn-for-service :- ServiceStatus
+(schema/defn ^:always-validate call-latest-status-fn-for-service :- ServiceStatus
   "Given a list of maps containing service information and a status function,
   find the latest version, and return a map with the service's version, the
   version of the service's status, and the results of calling this status
   function."
   [service :- [ServiceInfo]
-   level]
+   level :- ServiceStatusDetailLevel]
   (let [latest-status (last (sort-by :service-status-version service))]
     (assoc (select-keys latest-status [:service-version :service-status-version])
            :status ((:status-fn latest-status) level))))
 
-(schema/defn call-status-fns :- ServicesStatus
+(schema/defn ^:always-validate call-status-fns :- ServicesStatus
   "Call the latest status function for each service in the service context,
   and return a map of service to service status."
-  [status-fns-atom level]
+  [status-fns :- ServicesInfo
+   level :- ServiceStatusDetailLevel]
   (into {} (pmap (fn [[k v]] {k (call-latest-status-fn-for-service v level)})
-                 (deref status-fns-atom))))
+                 status-fns)))
 
 (defn get-status-detail-level
   "Given a params map from a request, get out the status level and check
@@ -86,7 +87,7 @@
       (compojure/context "/v1" []
         (compojure/GET "/services" [:as {params :query-params}]
           (let [level (get-status-detail-level params)
-                statuses (call-status-fns status-fns-atom level)]
+                statuses (call-status-fns (deref status-fns-atom) level)]
             {:status 200
              :body statuses}))
          (compojure/GET "/services/:service-name" [service-name :as {params :query-params}]
@@ -103,5 +104,6 @@
 (defn build-handler [status-fns-atom]
   (-> (build-routes status-fns-atom)
       ringutils/wrap-request-data-errors
+      ringutils/wrap-schema-errors
       ringutils/wrap-errors
       ring-json/wrap-json-response))
