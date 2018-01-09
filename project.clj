50a3482431bfa48184657bedecd80e320596f8f6
Justin Stoller <justin.stoller@gmail.com>
2018-01-09 14:12:38 -0800

    (TK-458) Update version and CHANGELOG for 1.0.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/trapperkeeper-status "0.7.2-SNAPSHOT"
+(defproject puppetlabs/trapperkeeper-status "1.0.0-SNAPSHOT"
   :description "A trapperkeeper service for getting the status of other trapperkeeper services."
   :url "https://github.com/puppetlabs/trapperkeeper-status"
   :license {:name "Apache License, Version 2.0"
