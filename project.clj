b6910cc20b2551cedea7dcc0d253d57fe01c2ccd
Jenkins CI <ci@puppet.com>
2020-08-31 19:15:55 +0000

    Version 1.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/trapperkeeper-status "1.1.1"
+(defproject puppetlabs/trapperkeeper-status "1.1.2-SNAPSHOT"
   :description "A trapperkeeper service for getting the status of other trapperkeeper services."
   :url "https://github.com/puppetlabs/trapperkeeper-status"
   :license {:name "Apache License, Version 2.0"
